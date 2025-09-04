48b79f133b3823f61d4ac7410b78322dac653212
PublicDavid <96501934+PublicDavid@users.noreply.github.com>
2025-09-04 20:40:21 +0200

    Seperate files for Github Actions

---
+++ check_match.py
@@ -0,0 +1,109 @@
+import os
+import requests
+import time
+import pytz
+import json
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from datetime import datetime, timedelta, timezone
+
+# CONFIG VARIABLES
+DISCORD_BOT = os.getenv("DISCORD_BOT")
+TEAM = "Juventus"
+TEAM_ID = 133676  # Juventus ID in TheSportsDB
+TIMEZONE = "Europe/Brussels"
+#Github Actions Token
+GH_TOKEN = os.environ.get("GH_TOKEN")
+GH_REPO = os.environ.get("GH_REPO")
+
+# --- sends embed to discord channel ---
+def send_discord_notification(embed):
+    try:
+        webhook = DiscordWebhook(url=DISCORD_BOT, rate_limit_retry=True)
+        webhook.add_embed(embed)
+        response = webhook.execute()
+        print("Notification sent to Discord.")
+    except Exception as e:
+        print(f"Error sending Discord notification: {e}")
+
+# --- sends a reminder a day before the match ---
+def send_daily_reminder(match):
+    global daily_reminder_sent_for_date
+
+    match_date_str = match['dateEvent']
+
+    # check if reminder has already been sent for this match date
+    if daily_reminder_sent_for_date == match_date_str:
+        print(f"Reminder already sent for match on {match_date_str}.")
+        return
+    
+    if match['strHomeTeam'] == TEAM:
+        opponent = match['strAwayTeam']
+        opponent_logo = match.get('strAwayTeamBadge')
+    else:
+        opponent = match['strHomeTeam']
+        opponent_logo = match.get('strHomeTeamBadge')
+
+    competition = match['strLeague']
+
+    datetime_str = f"{match['dateEvent']} {match['strTime']}"
+    match_datetime_utc = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+
+    local_timezone = pytz.timezone(TIMEZONE)
+    fixture_local = match_datetime_utc.astimezone(local_timezone)
+
+    embed = DiscordEmbed(title="⚽ Matchday tomorrow! ⚽", description=f"Tomorrow ({fixture_local.strftime('%d-%m-%Y')}) Juventus plays a game in **{competition}**." ,color='000000')
+    if opponent_logo:
+        embed.set_thumbnail(url=opponent_logo)
+    else:
+        embed.set_thumbnail(url="https://nl.wikipedia.org/wiki/Juventus_FC#/media/Bestand:Juventus_FC_-_logo_black_(Italy,_2020).svg")
+    embed.add_embed_field(name="Opponent", value=opponent)
+    embed.add_embed_field(name="Time", value=f"{fixture_local.strftime('%H:%M')}")
+    embed.set_footer(text="Fino alla fine! 🖤🤍")
+
+    send_discord_notification(embed)
+    daily_reminder_sent_for_date = match_date_str
+
+# updates a github variable
+def update_github_variable(variable_name, value):
+    url = f"https://api.github.com/repos/{GH_REPO}/actions/variables/{variable_name}"
+    headers = {"Authorization": f"token {GH_TOKEN}", "Accept": "application/vnd.github.v3+json"}
+    data = {"name": variable_name, "value": json.dumps(value)}
+    response = requests.patch(url, json=data)
+    if response.status_code == 204:
+        print(f"Github variable '{variable_name}' updated.")
+    else:
+        print(f"Error updating Github variable: {response.status_code} - {response.text}")
+        raise Exception("Can't find Github variable.")
+    
+# find the next match and update the variable
+def find_next_match():
+    print("Searching next game...")
+    url = f"https://www.thesportsdb.com/api/v1/json/123/eventsnext.php?id={TEAM_ID}"
+    try:
+        response = requests.get(url).json()
+        if response.get('events'):
+            match = response['events'][0]
+            print(f"Found next game: {match['strEvent']} op {match['dateEvent']}")
+            
+            match_info_to_store = {
+                "idEvent": match['idEvent'],
+                "dateEvent": match['dateEvent'],
+                "strTime": match['strTime'],
+                "strEvent": match['strEvent']
+            }
+            update_github_variable('NEXT_MATCH_INFO', match_info_to_store)
+
+            # Logic for daily reminders
+            now_utc = datetime.now(timezone.utc)
+            match_time_utc = datetime.strptime(f"{match['dateEvent']} {match['strTime']}", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+            if now_utc.date() == (match_time_utc.date() - timedelta(days=1)):
+                send_daily_reminder(match)
+        else:
+            print("Didn't find any upcoming games, resetting variable.")
+            update_github_variable('NEXT_MATCH_INFO', {})
+
+    except Exception as e:
+        print(f"Error while fetching next game: {e}")
+
+if __name__ == "__main__":
+    find_next_match()
---
+++ send_score.py
@@ -0,0 +1,123 @@
+# send_score.py
+import os
+import requests
+import json
+import pytz
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from datetime import datetime, timedelta, timezone
+
+# --- CONFIGURATIE (hetzelfde als in check_match.py) ---
+DISCORD_BOT = os.environ.get("DISCORD_BOT")
+GH_TOKEN = os.environ.get("GH_TOKEN")
+GH_REPO = os.environ.get("GH_REPO")
+BASE_URL = "https://www.thesportsdb.com/api/v1/json/123/"
+TEAM = "Juventus"
+TIMEZONE = "Europe/Brussels"
+
+# --- sends embed to discord channel ---
+def send_discord_notification(embed):
+    try:
+        webhook = DiscordWebhook(url=DISCORD_BOT, rate_limit_retry=True)
+        webhook.add_embed(embed)
+        response = webhook.execute()
+        print("Notification sent to Discord.")
+    except Exception as e:
+        print(f"Error sending Discord notification: {e}")
+
+# --- shows final score of game ---
+def send_final_score(match_id):
+    url = f"{BASE_URL}lookupevent.php?id={match_id}"
+
+    print("Fetching final score for match ID:", match_id)
+
+    try:
+        response = requests.get(url).json()
+
+        if response.get('events'):
+            match = response['events'][0]
+
+            # check if the game is finished
+            if match.get('strStatus') == 'Match Finished':
+                home_team = match['strHomeTeam']
+                away_team = match['strAwayTeam']
+                home_score = match['intHomeScore']
+                away_score = match['intAwayScore']
+                competition = match['strLeague']
+
+                if match['strHomeTeam'] == TEAM:
+                    opponent = match['strAwayTeam']
+                    opponent_logo = match.get('strAwayTeamBadge')
+                else:
+                    opponent = match['strHomeTeam']
+                    opponent_logo = match.get('strHomeTeamBadge')
+
+                embed = DiscordEmbed(title="🏆 Match Ended! 🏆", description=f"Juventus played a game in **{competition}**." ,color='000000')
+                if opponent_logo:
+                    embed.set_thumbnail(url=opponent_logo)
+                else:
+                    embed.set_thumbnail(url="https://nl.wikipedia.org/wiki/Juventus_FC#/media/Bestand:Juventus_FC_-_logo_black_(Italy,_2020).svg")
+                embed.add_embed_field(name="Opponent", value=opponent)
+                embed.add_embed_field(name="Final Score", value=f"{home_team} {home_score} - {away_score} {away_team}")
+                embed.set_footer(text="Fino alla fine! 🖤🤍")
+
+                send_discord_notification(embed)
+                print("Final score notification sent.")
+                return True
+            else:
+                print("No match details found for final score.")
+
+    except Exception as e:
+        print(f"Error fetching final score: {e}")
+
+    return False
+
+# updates a github variable
+def update_github_variable(variable_name, value):
+    url = f"https://api.github.com/repos/{GH_REPO}/actions/variables/{variable_name}"
+    headers = {"Authorization": f"token {GH_TOKEN}", "Accept": "application/vnd.github.v3+json"}
+    data = {"name": variable_name, "value": json.dumps(value)}
+    response = requests.patch(url, json=data)
+    if response.status_code == 204:
+        print(f"Github variable '{variable_name}' updated.")
+    else:
+        print(f"Error updating Github variable: {response.status_code} - {response.text}")
+        raise Exception("Can't find Github variable.")
+    
+
+def get_github_variable(variable_name):
+    url = f"https://api.github.com/repos/{GH_REPO}/actions/variables/{variable_name}"
+    headers = {"Authorization": f"token {GH_TOKEN}", "Accept": "application/vnd.github.v3+json"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()['value']
+    else:
+        raise Exception(f"Can't read Github variable: {response.text}")
+
+# Checks if game has ended and sends final score
+def check_and_send_final_score():
+    print("Checking if final score is avaialable...")
+    match_info_str = get_github_variable('NEXT_MATCH_INFO')
+    
+    if not match_info_str or match_info_str == "{}":
+        print("No game available.")
+        return
+
+    match_info = json.loads(match_info_str)
+    
+    datetime_str = f"{match_info['dateEvent']} {match_info['strTime']}"
+    match_time_utc = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+    match_end_time = match_time_utc + timedelta(minutes=130)
+    
+    if datetime.now(timezone.utc) > match_end_time:
+        print(f"Game {match_info['idEvent']} has ended. Fetching final score...")
+        success = send_final_score(match_info['idEvent'])
+        if success:
+            print("Final score has been send._Resetting Github variable.")
+            update_github_variable('NEXT_MATCH_INFO', {})
+        else:
+            print("Final score not available yet, trying again in 15 minutes.")
+    else:
+        print("Game not finished yet.")
+
+if __name__ == "__main__":
+    check_and_send_final_score()
